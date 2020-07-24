4b78dec617aa06e0d736e764cb9a94bf757ec215
huda <danhm@gmail.com>
2020-07-23 22:46:54 -0500

    rutas author
    se añadió una nueva ruta y nuevas views

--- server.js
+++ server.js
@@ -6,17 +6,20 @@ const express = require("express");
 const app = express();
 const expressLayouts = require("express-ejs-layouts");
 const moongoose = require("mongoose");
+const bodyParser = require('body-parser');
+
 
 app.set("view engine", "ejs");
 app.set("views", __dirname + "/views");
 app.set("layout","layouts/layout");
 app.use(expressLayouts);
 app.use(express.static("public"));
+app.use(bodyParser.urlencoded({limit:'10mb', extended:false}));
 
 
 const indexRouter = require("./routes/index.js");
-
-moongoose.connect(process.env.DATABASE_URL, {useNewUrlParser:true});
+const authorRouter = require('./routes/author.js');
+moongoose.connect(process.env.DATABASE_URL, {useUnifiedTopology:true});
 
 const db = moongoose.connection;
 
@@ -24,6 +27,7 @@ db.on("error",(error)=> console.error(error));
 db.once("open",()=> console.log("connected to moongoose"));
 
 app.use("/", indexRouter);
+app.use("/authors", authorRouter);
 
 app.listen(process.env.PORT || 3000);
 
